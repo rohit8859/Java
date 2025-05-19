f66cca7205939f9adf69eb9e5c646b2f3ade7483
rohit8859 <rohitkumar885985@gmail.com>
2025-05-20 00:36:03 +0530

    pattern4

---
+++ pattern4.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class pattern4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter the number of rows for the pyramid pattern: ");
+        int n=sc.nextInt();
+        for(int i=1; i<=n; i++) {
+            for(int space=1; space<=n-i; space++) {
+                System.out.print(" ");
+            }
+            for(int j=1; j<=(2*i-1); j++) {
+                System.out.print("*");
+            }
+            System.out.println( );
+        }
+    }
+}
